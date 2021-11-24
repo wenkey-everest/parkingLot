52adf9afa64e6934fe988206621500b3d4f600e9
wenkey <muni.v.g@everest.engineering>
2021-11-24 17:21:34 +0530

    Added test for checking number of slots in floor

---
+++ src/test/java/com/everest/parkinglot/models/LevelTest.java
@@ -0,0 +1,13 @@
+package com.everest.parkinglot.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelTest {
+    @Test
+    public void testingLevel(){
+        Level level = new Level(6);
+        assertEquals(level.getSpots().size(),6,"Slots in floor are not created with user input");
+    }
+}
