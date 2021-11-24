2dc5df804cbd30cc8f7525312ebb707dcb35f20b
wenkey <muni.v.g@everest.engineering>
2021-11-24 14:26:06 +0530

    Displaying freeslots, freecount, occupied slots

---
+++ src/main/java/com/everest/parkinglot/Display.java
@@ -0,0 +1,55 @@
+package com.everest.parkinglot;
+import com.everest.parkinglot.services.ParkingLot;
+import com.everest.parkinglot.models.ParkingSpot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Display {
+    private int count=0;
+    public void displayFreeCount(ParkingLot parkingLot, String vehicleType){
+        for(int i =0;i<parkingLot.getNumberOfFloor();i++){
+            count=0;
+            for (int j=0;j<parkingLot.getNumberOfSlotsPerFloor();j++) {
+                ParkingSpot spot = parkingLot.getLevels(i).get(j);
+                if(spot.getSpotType().toString().equalsIgnoreCase(vehicleType)){
+                    if(spot.getStatus().equalsIgnoreCase("free")) {
+                        count++;
+                    }
+                }
+            }
+            System.out.println("No. of free slots for " + vehicleType + " on Floor " +(i+1) + " : " +count);
+
+        }
+        }
+    public void displayFreeSlots(ParkingLot parkingLot, String vehicleType) {
+        for(int i =0;i<parkingLot.getNumberOfFloor();i++){
+            List<Integer> count = new ArrayList<>();
+            for (int j=0;j<parkingLot.getNumberOfSlotsPerFloor();j++) {
+                ParkingSpot spot = parkingLot.getLevels(i).get(j);
+                if(spot.getSpotType().toString().equalsIgnoreCase(vehicleType)){
+                    if(spot.getStatus().equalsIgnoreCase("free")) {
+                        count.add(spot.getSpotNumber());
+                    }
+                }
+            }
+            System.out.println("Free slots for " + vehicleType + " on Floor " +(i+1)+ " : "+count.toString().replace("[","").replace("]",""));
+        }
+    }
+
+    public void displayOccupiedSlots(ParkingLot parkingLot, String vehicleType) {
+        for(int i =0;i<parkingLot.getNumberOfFloor();i++){
+            count=0;
+            for (int j=0;j<parkingLot.getNumberOfSlotsPerFloor();j++) {
+                ParkingSpot spot = parkingLot.getLevels(i).get(j);
+                if(spot.getSpotType().toString().equalsIgnoreCase(vehicleType)){
+                    if(spot.getStatus().equalsIgnoreCase("occupied")) {
+                        count++;
+                    }
+                }
+            }
+            System.out.println("Occupied slots for " + vehicleType + " on Floor " +(i+1) + " : " +count);
+        }
+    }
+}
