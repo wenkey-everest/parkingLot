e11b06c4c9df5ee710fdcbe7add464f38c5305b7
wenkey <muni.v.g@everest.engineering>
2021-11-25 16:08:13 +0530

    removed duplicate code in display

--- src/main/java/com/everest/parkinglot/Display.java
+++ src/main/java/com/everest/parkinglot/Display.java
@@ -1,27 +1,17 @@
 package com.everest.parkinglot;
+
 import com.everest.parkinglot.services.ParkingLot;
 import com.everest.parkinglot.models.ParkingSpot;
+
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class Display {
     private int count = 0;
     private String tempCount;
-    public int displayFreeCount(ParkingLot parkingLot, String vehicleType) {
-        for (int i = 0; i < parkingLot.getNumberOfFloor(); i++) {
-            count = 0;
-            for (int j = 0; j < parkingLot.getNumberOfSlotsPerFloor(); j++) {
-                ParkingSpot spot = parkingLot.getLevels(i).get(j);
-                if (spot.getSpotType().toString().equalsIgnoreCase(vehicleType)) {
-                    if (spot.getStatus().equalsIgnoreCase("free")) {
-                        count++;
-                    }
-                }
-            }
-            System.out.println("No. of free slots for " + vehicleType + " on Floor " + (i + 1) + " : " + count);
 
-        }
+    public int displayFreeCount(ParkingLot parkingLot, String vehicleType) {
+        displayAdapter(parkingLot, vehicleType, "free", "No. of free slots for");
         return count;
     }
 
@@ -36,25 +26,29 @@ public class Display {
                     }
                 }
             }
-            tempCount =  count.toString().replace("[", "").replace("]", "");
+            tempCount = count.toString().replace("[", "").replace("]", "");
             System.out.println("Free slots for " + vehicleType + " on Floor " + (i + 1) + " : " + tempCount);
         }
         return tempCount;
     }
 
     public int displayOccupiedSlots(ParkingLot parkingLot, String vehicleType) {
+        displayAdapter(parkingLot, vehicleType, "occupied", "Occupied slots for");
+        return count;
+    }
+
+    public void displayAdapter(ParkingLot parkingLot, String vehicleType, String status, String inputString) {
         for (int i = 0; i < parkingLot.getNumberOfFloor(); i++) {
             count = 0;
             for (int j = 0; j < parkingLot.getNumberOfSlotsPerFloor(); j++) {
                 ParkingSpot spot = parkingLot.getLevels(i).get(j);
                 if (spot.getSpotType().toString().equalsIgnoreCase(vehicleType)) {
-                    if (spot.getStatus().equalsIgnoreCase("occupied")) {
+                    if (spot.getStatus().equalsIgnoreCase(status)) {
                         count++;
                     }
                 }
             }
-            System.out.println("Occupied slots for " + vehicleType + " on Floor " + (i + 1) + " : " + count);
+            System.out.println(inputString + " " + vehicleType + " on Floor " + (i + 1) + " : " + count);
         }
-        return count;
     }
 }
--- src/test/java/com/everest/parkinglot/models/LevelTest.java
+++ src/test/java/com/everest/parkinglot/models/LevelTest.java
@@ -1,5 +1,6 @@
 package com.everest.parkinglot.models;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,4 +11,5 @@ class LevelTest {
         Level level = new Level(6);
         assertEquals(level.getSpots().size(),6,"Slots in floor are not created with user input");
     }
+
 }
